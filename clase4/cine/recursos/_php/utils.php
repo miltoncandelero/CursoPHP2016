5c5a53031af448a06c9ce73b875aafd90c2202e3
Milton Candelero <Milton Candelero>
2016-06-11 10:05:56 -0300

    agregados los model y controller de pelicula

--- clase4/cine/recursos/_php/utils.php
+++ clase4/cine/recursos/_php/utils.php
@@ -4,3 +4,26 @@
 		 var_dump($data);
 		 echo "</PRE>";
 	 }
+	 
+	 	 //Dibujar cualquier tabla.
+	 function tabledraw($sTable){
+		 foreach ($sTable as $Fila) {    
+			 foreach ($Fila as $Celda) {
+    			echo $Celda. "\t|\t";
+			 }
+			 echo "<br>";
+		}
+	 }
+		
+	//Dibujar cualquier tabla.
+	 function tabledrawhtml($sTable){
+		 echo "<br><table style=\"border: 1px solid black; border-collapse: collapse;\">";
+		 foreach ($sTable as $Fila) {   
+			 echo "<tr style=\"border: 1px solid black;\">"; 
+			 foreach ($Fila as $Celda) {
+    			echo "<td style=\"border: 1px solid black;\">".$Celda. "</td>";
+			 }
+			 echo "</tr>";
+		}
+		echo "</table>";
+	 }
