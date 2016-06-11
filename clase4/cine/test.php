b78ce60cdf2e03d51adca9ccba5aa2eef14cd784
Milton Candelero <Milton Candelero>
2016-06-11 11:48:19 -0300

    Final commit del branch me duele todo

--- clase4/cine/test.php
+++ clase4/cine/test.php
@@ -2,17 +2,16 @@
 include("inc.includes.php");
 $db = new MySQL($config["dbhost"],$config["dbuser"],$config["dbpass"],$config["db"]);
 
-
-/*$oPeliculaController = new PeliculaController();
-echo $oPeliculaController->ListarPeliculas();*/
-
-
-
-/*$oPeliculaController = new PeliculaController();
-echo $oPeliculaController->PeliculasPorDirector();
-*/
-
 $oPeliculaController = new PeliculaController();
+echo $oPeliculaController->ListarPeliculas();
+
+
+
+//$oPeliculaController = new PeliculaController();
+echo $oPeliculaController->PeliculasPorDirector();
+
+
+//$oPeliculaController = new PeliculaController();
 echo $oPeliculaController -> PeliculasPorGenero();
 
 /*
